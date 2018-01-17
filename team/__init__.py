8a69d39928817ca485936d961ab9a4706a68ed5c
Rike-Benjamin Schuppner <rikebs@debilski.de>
2018-01-17 12:10:25 +1100

    BF: Missed a few renames.

--- team/__init__.py
+++ team/__init__.py
@@ -3,19 +3,19 @@ from pelita.player import SimpleTeam
 from .demo_player import KangarooPlayer
 # (please use relative imports inside your module)
 
-# The default factory method, which this module must export.
+# The default team factory method, which this module must export.
 # It must return an instance of `SimpleTeam`  containing
 # the name of the team and the respective instances for
 # the first and second player.
 
 def team():
-    return SimpleTeam("Drunk Team", KangarooPlayer(), KangarooPlayer())
+    return SimpleTeam("Kangaroo Team", KangarooPlayer(), KangarooPlayer())
 
 # For testing purposes, one may use alternate factory methods::
 #
-#     def alternate_factory():
+#     def alternate_team():
 #          return SimpleTeam("Our alternate Team", AlternatePlayer(), AlternatePlayer())
 #
 # To be used as follows::
 #
-#     $ pelita path_to/groupN/:alternate_factory
+#     $ pelita path_to/groupN/:alternate_team
